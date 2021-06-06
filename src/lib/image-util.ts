60afd28a46edb79457fb84bf27b74c19fdc47b48
slaurent <slaurent22@pm.me>
2021-06-06 09:31:02 -0400

    closer, i think

--- src/lib/image-util.ts
+++ src/lib/image-util.ts
@@ -1,5 +1,7 @@
 import logo from "../asset/image/logo.png";
 
+const sliceAt = 7485;
+
 // eslint-disable-next-line max-len
 const LIVESPLIT_ICON_DATA_HEADER = "AAEAAAD/////AQAAAAAAAAAMAgAAAFFTeXN0ZW0uRHJhd2luZywgVmVyc2lvbj00LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPWIwM2Y1ZjdmMTFkNTBhM2EFAQAAABVTeXN0ZW0uRHJhd2luZy5CaXRtYXABAAAABERhdGEHAgIAAAAJAwAAAA8DAAAA";
 const extraHeader = "Nh0AAA"; // TODO: what
@@ -24,16 +26,39 @@ function groupBy8(str: string): RegExpMatchArray {
     return match;
 }
 
+function hexByteStr(n: number): string {
+    return n.toString(16).padStart(2, "0");
+}
+
+function logHex(foo: Array<number>): void {
+    console.log(foo.map(hexByteStr).slice(sliceAt));
+}
+
 function binaryTransform(uint8Array: Uint8Array, transform: (byteStr: string) => string) {
-    const binStr = bitString(uint8Array);
-    const transformedBinStr = transform(binStr);
+    console.log("=================");
+    // const binStr = bitString(uint8Array);
+    // const binStr = uint8Array.join("");
+    console.log(uint8Array.slice(sliceAt));
+    logHex(Array.from(uint8Array));
+    const bytes = Array.from(uint8Array).map((char, index) => String.fromCharCode(index));
+    const joinedBinStr = Array.from(uint8Array).map(binaryByteStr).join("");
+    console.log(groupBy8(joinedBinStr).slice(sliceAt).map(n => parseInt(n, 2)));
+    const transformedBinStr = transform(joinedBinStr);
     const groupedTransformedBinStr = groupBy8(transformedBinStr);
+    console.log(groupedTransformedBinStr.slice(sliceAt));
     const transformedBytes = groupedTransformedBinStr.map(n => parseInt(n, 2));
+    console.log(transformedBytes.slice(sliceAt));
+    logHex(transformedBytes);
+
+    transformedBytes[transformedBytes.length - 1] = 0x20;
+    logHex(transformedBytes);
+    console.log("=================");
     return Uint8Array.from(transformedBytes);
 }
 
 function shiftIntoLivesplitFormat(uint8Array: Uint8Array): Uint8Array {
     return binaryTransform(uint8Array, binStr => `0010${binStr}`);
+    // return binaryTransform(uint8Array, binStr => binStr);
 }
 
 function toBase64(uint8Array: Uint8Array): string {
@@ -65,11 +90,15 @@ function liveSplitifyThePNG(uint8Array: Uint8Array): Uint8Array {
 }
 
 export async function createLiveSplitIconData(imageUrl: string): Promise<string> {
-    console.log(imageUrl);
+    // console.log("imageBuffer, uint8Array, fuckyWuckyUint8Array, shiftedArray");
     const imageBuffer = await fetchAsArrayBuffer(imageUrl);
-    const uint8Array = liveSplitifyThePNG(new Uint8Array(imageBuffer));
-    const shiftedArray = shiftIntoLivesplitFormat(uint8Array);
-    console.log(shiftedArray);
+    // console.log(imageBuffer);
+    const uint8Array = new Uint8Array(imageBuffer);
+    console.log(uint8Array.slice(sliceAt));
+    const fuckyWuckyUint8Array = uint8Array // liveSplitifyThePNG(uint8Array);
+    // console.log(fuckyWuckyUint8Array.slice(sliceAt));
+    const shiftedArray = shiftIntoLivesplitFormat(fuckyWuckyUint8Array);
+    console.log(shiftedArray.slice(sliceAt));
     const base64 = toBase64(shiftedArray);
     return `${LIVESPLIT_ICON_DATA_HEADER}${extraHeader}${base64}`;
 
